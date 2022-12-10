14a5f710611227cc6e09e9c9d8b16706fad1f54a
rupam swain <rupamswain1@gmail.com>
2022-12-11 00:19:59 +0530

    added functionality to swipe out or to remain in same postion if user dosent swipe completely

--- src/components/Deck/Deck.js
+++ src/components/Deck/Deck.js
@@ -1,7 +1,17 @@
-import { View, Text, StyleSheet, Animated, PanResponder, Dimensions } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Animated,
+  PanResponder,
+  Dimensions,
+} from 'react-native';
 
 import React, { useState } from 'react';
-const Deck = ({ data, renderCard }) => {
+const Deck = ({ data, renderCard, onSwipeRight, onSwipeLeft }) => {
+  const [swipableCard,setSwipableCard]=useState(0);
+  const SCREEN_WIDTH = Dimensions.get('window').width;
+  const SWIPE_THRESHOLD=0.25*SCREEN_WIDTH;
   const position = React.useRef(new Animated.ValueXY()).current;
   const panResponder = React.useRef(
     PanResponder.create({
@@ -9,32 +19,61 @@ const Deck = ({ data, renderCard }) => {
       onPanResponderMove: (event, gesture) => {
         position.setValue({ x: gesture.dx, y: gesture.dy });
       },
-      onPanResponderRelease: () => {},
+      onPanResponderRelease: (event,gesture) => {
+        if(gesture.dx>SWIPE_THRESHOLD){
+          forcedSwipe('right')
+        }
+        else if(gesture.dx<-SWIPE_THRESHOLD){
+          forcedSwipe('left')
+        }
+        else{
+          resetCardPosition();
+        }
+        
+      },
     })
   ).current;
-  const SCREEN_WIDTH=Dimensions.get('window').width;
- const getCartdStyle=()=>{
-  const rotate=position.x.interpolate({
-    inputRange:[-SCREEN_WIDTH*1.5,0,SCREEN_WIDTH*1.5],
-    outputRange:['-120deg','0deg','120deg']
-  })
-  return {
-    ...position.getLayout(),
-    transform:[{rotate}]
+
+  const forcedSwipe=(direction)=>{
+    const x=direction==='right'?SCREEN_WIDTH:-SCREEN_WIDTH;
+    Animated.spring(position,{
+      toValue:{x,y:0},
+      duration:250
+    }).start(()=>onSwipeComplete(direction));
   }
- }
+
+  const onSwipeComplete=(direction)=>{
+    const item=data[swipableCard]
+    direction==='right'?onSwipeRight():onSwipeLeft();
+  }
+
+  const resetCardPosition = () => {
+    Animated.spring(position, {
+      toValue: { x: 0, y: 0 },
+    }).start();
+  };
+  const getCartdStyle = () => {
+    const rotate = position.x.interpolate({
+      inputRange: [-SCREEN_WIDTH * 1.5, 0, SCREEN_WIDTH * 1.5],
+      outputRange: ['-120deg', '0deg', '120deg'],
+    });
+    return {
+      ...position.getLayout(),
+      transform: [{ rotate }],
+    };
+  };
   const renderCards = () => {
     return data.map((item, index) => {
-      return (
-        index==0?
+      return index == 0 ? (
         <Animated.View
           key={index}
           style={getCartdStyle()}
           {...panResponder.panHandlers}
         >
-         { renderCard(item)}
+          {renderCard(item)}
         </Animated.View>
-        :<>{renderCard(item)}</>
+      ) : (
+        <>{renderCard(item)}</>
       );
     });
   };
