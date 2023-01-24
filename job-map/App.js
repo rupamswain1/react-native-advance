05a77ee080ad2e05d726251d105f502c4e8c2b87
rupam swain <rupamswain1@gmail.com>
2023-01-24 23:51:48 +0530

    navigation added

--- job-map/App.js
+++ job-map/App.js
@@ -1,11 +1,62 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
 
+import AuthScreen from './src/screens/AuthScreen';
+import Welcome from './src/screens/WelcomeScreen';
+import MapScreen from './src/screens/MapScreen';
+import DeckScreen from './src/screens/DeckScreen';
+import ReviewScreen from './src/screens/ReviewScreen';
+import SettingScreen from './src/screens/SettingScreen';
+const Tab = createBottomTabNavigator();
+// const MainTab=createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+const ReviewComponent=()=>{
+  return(
+    <Stack.Navigator>
+      <Stack.Screen name="Review"
+       component={ReviewScreen} 
+      //  options={{title:"Review Jobs",
+      //  headerTitleAlign:'center',
+      //  headerRight:()=>{
+      //   return <Text>Go Back</Text>
+      //  }
+      // }}
+      />
+      <Stack.Screen name="Setting" component={SettingScreen} />
+   </Stack.Navigator>
+  )
+}
+
+const MainTabConponent=()=>{
+  return(
+    <Tab.Navigator>
+    <Tab.Screen name="map" component={MapScreen}/>
+    <Tab.Screen name="deck" component={DeckScreen}/>
+    <Tab.Screen name="review" component={ReviewComponent}
+      options={{headerShown:false}}
+    />
+  </Tab.Navigator>
+  )
+}
 export default function App() {
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <NavigationContainer>
+      <Tab.Navigator >
+        <Tab.Screen name="welcome" component={Welcome} 
+          options={{headerShown:false}}
+        />
+        <Tab.Screen name="auth" component={AuthScreen} />
+        <Tab.Screen name="main" component={MainTabConponent}
+          options={{headerShown:false}}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
     </View>
   );
 }
@@ -14,7 +65,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
   },
 });
--- job-map/src/screens/ReviewScreen.jsx
+++ job-map/src/screens/ReviewScreen.jsx
@@ -1,6 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { View, Text } from 'react-native';
-const ReviewScreen = () => {
+import { Button } from 'react-native-elements';
+const ReviewScreen = ({ navigation }) => {
+    useEffect(() => {
+        navigation.setOptions({
+            title: "Review Jobs",
+            headerTitleAlign: 'center',
+            headerRight: ({ navigate }) => {
+                return <Button title="Settings"
+                    onPress={() => navigation.navigate("Setting")}
+                    buttonStyle={{ color: "rgba(0,0,0,0)" }}
+
+                    color="rgba(0,122,255,1"
+                />
+            },
+            headerStyle: {
+                backgroundColor: '#dfdfdf'
+            }
+        })
+    }, [])
     return (
         <View>
             <Text>Review Screen</Text>
