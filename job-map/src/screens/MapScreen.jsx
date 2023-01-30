c3dfba13bf3fc6e3235b8a32b5f1aacca727ab3d
rupam swain <rupamswain1@gmail.com>
2023-01-30 22:54:12 +0530

    map is added

--- job-map/src/screens/MapScreen.jsx
+++ job-map/src/screens/MapScreen.jsx
@@ -1,11 +1,50 @@
-import React from 'react'
-import { View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react'
+import { View, Text, StyleSheet, ActivityIndicator } from 'react-native';
+
+import MapView from 'react-native-maps';
+
 const MapScreen = () => {
+    const [region, setRegion] = useState({
+        longitude: -122,
+        latitude: 37,
+        longitudeDelta: 0.04,
+        latitudeDelta: 0.09
+    })
+    const [mapLoaded, setMapLoaded] = useState(false)
+    useEffect(() => {
+        setMapLoaded(true)
+    }, [])
+    const updateRegion = (location) => {
+        console.log(location)
+        setRegion(location)
+    }
     return (
-        <View>
+        <View style={StyleSheet.container}>
             <Text>Map Screen</Text>
+            {
+                mapLoaded ?
+                    <MapView style={styles.mapStyle}
+                        region={region}
+                        onRegionChangeComplete={updateRegion}
+                    />
+                    :
+
+                    <ActivityIndicator size="large" />
+            }
+
         </View>
     )
 }
 
-export default MapScreen
+const styles = StyleSheet.create({
+    container: {
+        width: "100%",
+        height: "100%"
+    },
+    mapStyle: {
+        width: "100%",
+        height: "90%"
+    }
+})
+
+export default MapScreen 
